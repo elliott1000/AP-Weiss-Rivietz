ba89fb0e62f263cfbe3470ed8e676816807c7f13
elliott1000 <39814795+elliott1000@users.noreply.github.com>
2025-04-12 12:53:03 -0400

    Add files via upload

---
+++ apstuff.py
@@ -0,0 +1,47 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero is not allowed."
+def historyf():
+    print("Calculation History:")
+    if len(history) == 0:
+        print("No calculations yet.")
+        return
+    else:
+        for entry in history:
+            print(entry)
+    
+
+history = []
+
+while True:
+    operation = input("Enter the operation (+, -, *, /, or 'history' to view past results): ")
+    if operation == 'history':
+       historyf()
+       continue
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = subtract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    elif operation == '/':
+        result = divide(num1, num2)
+    print("Result:", result)
+    history.append(f"{num1} {operation} {num2} = {result}")
+
+
